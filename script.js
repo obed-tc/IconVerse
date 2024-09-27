8018c7978d791d2608466659e3b6abe829e74aed
obed-tc <castroobed53@gmail.com>
2024-09-27 19:08:44 -0400

    fix: Fixed file json

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-// Configura las rutas de las carpetas
-const iconFolder = path.join(__dirname, "icons"); // Ruta a la carpeta de íconos
-const outputFolder = path.join(__dirname, "output"); // Carpeta de salida para los JSON
-
-// Asegúrate de que la carpeta de salida exista
+const iconFolder = path.join(__dirname, "icons");
+const outputFolder = path.join(__dirname, "output");
 if (!fs.existsSync(outputFolder)) {
   fs.mkdirSync(outputFolder);
 }
@@ -13,7 +10,6 @@ if (!fs.existsSync(outputFolder)) {
 function getAllSvgFiles(dir) {
   const results = [];
 
-  // Lee los archivos en el directorio
   const files = fs.readdirSync(dir);
 
   files.forEach((file) => {
@@ -21,16 +17,14 @@ function getAllSvgFiles(dir) {
     const stat = fs.statSync(filePath);
 
     if (stat && stat.isDirectory()) {
-      // Si es un directorio, llama recursivamente
       results.push(...getAllSvgFiles(filePath));
     } else if (file.endsWith(".svg")) {
-      // Si es un archivo SVG, agrega su ruta
       const relativePath = path
         .relative(__dirname, filePath)
         .replace(/\\/g, "/"); // Cambia \\ por /
       results.push({
-        name: file.split(".")[0], // Nombre del ícono sin la extensión
-        url: `https://raw.githubusercontent.com/obed-tc/IconVerse/70606adc13f08e4051744d1258cb187c7d562c59/${relativePath}`, // Cambia la URL según tu repositorio
+        name: file.split(".")[0],
+        url: `${relativePath}`,
       });
     }
   });
@@ -39,10 +33,8 @@ function getAllSvgFiles(dir) {
 }
 
 function generateIconsJson(dir) {
-  // Obtener todos los archivos SVG en la carpeta actual
   const icons = getAllSvgFiles(dir);
 
-  // Solo generar el archivo JSON si hay íconos
   if (icons.length > 0) {
     const folderName = path.basename(dir);
     const outputFile = path.join(outputFolder, `${folderName}.json`);
@@ -50,7 +42,6 @@ function generateIconsJson(dir) {
     console.log(`JSON de íconos en ${folderName} generado en ${outputFile}`);
   }
 
-  // Obtener y procesar subcarpetas
   const subfolders = fs.readdirSync(dir).filter((file) => {
     const filePath = path.join(dir, file);
     return fs.statSync(filePath).isDirectory();
@@ -58,9 +49,8 @@ function generateIconsJson(dir) {
 
   subfolders.forEach((subfolder) => {
     const subfolderPath = path.join(dir, subfolder);
-    generateIconsJson(subfolderPath); // Llamada recursiva para subcarpetas
+    generateIconsJson(subfolderPath);
   });
 }
 
-// Ejecutar la función inicial para la carpeta de íconos
 generateIconsJson(iconFolder);
