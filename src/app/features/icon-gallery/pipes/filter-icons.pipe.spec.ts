70606adc13f08e4051744d1258cb187c7d562c59
obed-tc <castroobed53@gmail.com>
2024-09-27 17:47:36 -0400

    feat: Set up assets

--- src/app/features/icon-gallery/pipes/filter-icons.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterIconsPipe } from './filter-icons.pipe';
-
-describe('FilterIconsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterIconsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
