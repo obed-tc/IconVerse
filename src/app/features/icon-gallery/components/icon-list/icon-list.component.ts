f57ab0d3c3689293e4b0e936ecef3366cbdb3d36
obed-tc <castroobed53@gmail.com>
2024-09-28 12:52:41 -0400

    feat: Add search bar

--- src/app/features/icon-gallery/components/icon-list/icon-list.component.ts
+++ src/app/features/icon-gallery/components/icon-list/icon-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IconService } from 'src/app/core/services/icon.service';
+import { SearchService } from 'src/app/core/services/search.service';
 
 @Component({
   selector: 'app-icon-list',
@@ -8,18 +9,30 @@ import { IconService } from 'src/app/core/services/icon.service';
 })
 export class IconListComponent {
   icons: any[] = [];
+  filteredItems: any[]=[];
 
-  constructor(private iconService: IconService) { }
+
+  constructor(private iconService: IconService,private searchService: SearchService) { }
 
   ngOnInit(): void {
     this.iconService.getIcons().subscribe(
       data => {
         this.icons = data;
+        this.filteredItems=this.icons;
       },
       error => {
         console.error('Error al obtener los iconos', error);
       }
     );
+
+
+    this.searchService.search$.subscribe((term: string) => {
+        this.filteredItems = this.icons.filter(item =>
+          item.name.toLowerCase().includes(term.toLowerCase())
+  
+        );
+    });
+
   }
 
 
